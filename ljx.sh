e0ae832607d7608b2eb9b202ce4c0ac77f90fd09
ljx360 <79862310+ljx360@users.noreply.github.com>
2023-03-11 19:31:36 +0800

    Update ljx.sh

--- ljx.sh
+++ ljx.sh
@@ -2,7 +2,7 @@
 LJX_CONFIG=ljx.config
 CONFIG=/workdir/openwrt/.config
 
-if [ ! -f $CONFIG && -f $LJX_CONFIG ]; then
+if [ [ ! -f $CONFIG ] && [ -f $LJX_CONFIG ] ] ; then
 echo "cp [$LJX_CONFIG] TO [$CONFIG]"
 cp  $LJX_CONFIG $CONFIG
 else
