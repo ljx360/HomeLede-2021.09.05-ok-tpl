ec381b2fec29bf3f8f8f797e104fe5ac8c458ada
ljx360 <79862310+ljx360@users.noreply.github.com>
2023-03-11 16:20:24 +0800

    Create ljx.sh

---
+++ ljx.sh
@@ -0,0 +1,11 @@
+
+LJX_CONFIG=ljx.config
+CONFIG=/workdir/openwrt/.config
+
+if [ ! -f $CONFIG && -f $LJX_CONFIG ]; then
+echo "cp [$LJX_CONFIG] TO [$CONFIG]"
+cp  $LJX_CONFIG $CONFIG
+else
+echo "exist $CONFIG or miss $LJX_CONFIG"
+fi
+
